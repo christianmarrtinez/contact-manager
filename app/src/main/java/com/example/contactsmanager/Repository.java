17481a9f153b3ac650630f6125d8fbeda512c6af
christianmarrtinez <christianmarrtinez@gmail.com>
2024-12-07 13:53:03 -0500

    add handlers,exexcutors, and runnables to repository class

--- app/src/main/java/com/example/contactsmanager/Repository.java
+++ app/src/main/java/com/example/contactsmanager/Repository.java
@@ -1,6 +1,11 @@
 package com.example.contactsmanager;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Repository {
 
@@ -8,22 +13,50 @@ public class Repository {
     // -ROOM Database
 
     private final ContactDAO contactDAO;
+    ExecutorService executor;
+    Handler handler;
 
     public Repository(ContactDAO contactDAO) {
         this.contactDAO = contactDAO;
+
+        //USed for Background Database Operations
+        executor = Executors.newSingleThreadExecutor();
+
+        //Used for updating the UI
+        handler = new Handler(Looper.getMainLooper());
     }
 
     //Methods in DAO being executed from Repository
     public void addContact(Contacts contact){
-        contactDAO.insert(contact);
+
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.insert(contact);
+            }
+        });
+
     }
 
+
+
+
     public void deleteContact(Contacts contact){
-        contactDAO.delete(contact);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.delete(contact);
+            }
+        });
+
+
     }
 
     public List<Contacts> getAllContacts(){
         return contactDAO.getAllContacts();
     }
+
+
 
 }
